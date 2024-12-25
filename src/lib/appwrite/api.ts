d6426fd355aaee3b6ec3c0eb09d52c552bb1f50c
KaziMdMehediHasan <hassan.mehedee92@gmail.com>
2024-12-25 23:49:58 +0600

    create new post appwrite implemented

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
-import { INewUser } from "@/types";
-import { account, appwriteConfig, avatars, databases } from "./config";
-import { ID, Query } from "appwrite";
+import { INewPost, INewUser } from "@/types";
+import { account, appwriteConfig, avatars, databases, storage } from "./config";
+import { ID, ImageGravity, Query } from "appwrite";
 
 // a new user registration 
 export async function createUserAccount(user: INewUser) {
@@ -89,3 +89,105 @@ export const signOutAccount = async () => {
         console.log(error);
     }
 }
+
+// upload file to appwrite storage
+
+export const uploadFileToStorage = async (file: File) => {
+    try {
+        const uploadedFile = await storage.createFile(
+            appwriteConfig.storageId, //include the storage id where you want to upload the file
+            ID.unique(), //the unique id of the file
+            file //the file to upload
+        )
+        return uploadedFile;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// getting the uploaded file url
+
+export const getFileUrl = async (fileId: string) => {
+    try {
+        const fileUrl = await storage.getFilePreview(
+            appwriteConfig.storageId,
+            fileId,
+            2000,
+            2000,
+            ImageGravity.Top,
+            100
+        );
+
+        if (!fileUrl) throw Error;
+
+        return fileUrl;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// deleting the file from the storage
+export const deleteFileFromDB = async (fileId: string) => {
+    try {
+        await storage.deleteFile(
+            appwriteConfig.storageId,
+            fileId
+        )
+        return { status: 'Deletion successful' };
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// creating the post
+export const saveNewPostToDB = async (post: INewPost) => {
+    try {
+        // uploading file to appwrite storage
+        const uploadedFile = await uploadFileToStorage(post.file[0]);
+
+        if (!uploadedFile) throw Error;
+
+        //getting the file url
+
+        const fileUrl = getFileUrl(uploadedFile.$id);
+
+        // if the fileUrl is not available, delete the file from the storage and throw an error
+        if (!fileUrl) {
+            await deleteFileFromDB(uploadedFile.$id);
+            throw Error;
+        };
+
+        // converting tags to an array
+
+        const tags = post.tags?.replace(/ /g, "").split(",") || [];
+
+        // create the actual post
+
+        const newPost = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.postCollectionId,
+            ID.unique(),
+            {
+                creator: post.userId,
+                caption: post.caption,
+                imageUrl: fileUrl,
+                imageId: uploadedFile.$id,
+                location: post.location,
+                tags: tags
+            }
+        );
+
+        // this check ensures that the post is created successfully, if not, delete the file from the storage and throw an error
+        if (!newPost) {
+            await deleteFileFromDB(uploadedFile.$id);
+            throw Error;
+        }
+
+        return newPost;
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -18,3 +18,6 @@ export const useSignOutAccount = () => {
     })
 }
 
+export const useCreatePost = () => {
+
+}
